959a6eb0052d4ed680beddbdefccd0b0cf6dc381
pazbarda-nsknox <paz.barda@nsknox.net>
2026-05-25 08:42:27 +0300

    Message type + transcript extraction

--- AWS/lambda/VAPI_LAMBDA_WEBHOOK_HANDLER/vapi_webhook_handler.py
+++ AWS/lambda/VAPI_LAMBDA_WEBHOOK_HANDLER/vapi_webhook_handler.py
@@ -3,7 +3,15 @@ import json
 def lambda_handler(event, context):
 
     print("=== EVENT RECEIVED ===")
-    print(json.dumps(event))
+
+    print_event(event)
+    body = json.loads(event.get("body") or "{}")
+    message_type = get_message_type(body)
+    print(f"Message type: {message_type}")
+    if message_type == "end-of-call-report":
+        print_transcript(body)
+
+    print("=== END EVENT ===")
 
     return {
         'statusCode': 200,
@@ -11,3 +19,24 @@ def lambda_handler(event, context):
             'message': 'Hello from Lambda!'
         })
     }
+
+def print_event(event):
+    print("-- Event Dump:")
+    print(json.dumps(event))
+    print("-- End Event Dump")
+
+def get_message_type(body):
+    return body.get("message", {}).get("type")
+
+def print_transcript(body):
+    print("-- Transcript:")
+    print(extract_transcript(body))
+    print("-- End Transcript")
+
+def extract_transcript(body):
+    transcript = (
+        body.get("message", {})
+            .get("artifact", {})
+            .get("transcript")
+    )
+    return transcript
